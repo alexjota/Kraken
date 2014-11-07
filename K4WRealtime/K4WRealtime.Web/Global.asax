a185ef55a8430c3c79272511a7bf508701399fea
alexjota <alejandro.jezierski@southworks.net>
2014-11-07 19:47:28 -0300

    Initial Website

---
+++ K4WRealtime/K4WRealtime.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="K4WRealtime.Web.MvcApplication" Language="C#" %>
---
+++ K4WRealtime/K4WRealtime.Web/Hubs/AlertsHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace K4WRealtime.Web.Hubs
+{
+    public class AlertsHub : Hub
+    {
+        public void AlertReceived()
+        {
+            Clients.All.alertReceived();
+        }
+    }
+}
